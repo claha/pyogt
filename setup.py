4a6a43f8183bc429ac9993e6b2266e6719847899
Claes Hallström <hallstrom.claes@gmail.com>
2019-08-21 22:02:21 +0200

    Prepare for release 0.1.0 (#5)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyogt',
-    version='0.0.1',
+    version='0.1.0',
     author='Claes Hallstrom',
     author_email='hallstrom.claes@gmail.com',
     description='Retrieve departure information from OstgotaTrafiken',
@@ -11,7 +11,7 @@ setup(
     url='https://github.com/claha/pyogt',
     packages=find_packages(exclude=['tests', 'tests.*']),
     classifiers=[
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache Software License',
     ],
 )
